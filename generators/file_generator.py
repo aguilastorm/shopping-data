71719fbad6e2b016474aabe8a0cb19b123d49187
Lucas Tobon <lucas.tobon.v@gmail.com>
2023-05-20 14:11:33 -0500

    Test generator and Class soluction were completed

--- generators/file_generator.py
+++ generators/file_generator.py
@@ -1,2 +1,68 @@
+import csv
+
 class FileGenerator:
-    pass
+    def __init__(self, input_file):
+        self.input_file = input_file
+        self.data = self.import_csv()  # Import CSV data upon initialization
+
+    def import_csv(self):
+        data = {}
+        with open(self.input_file, "r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                category = row[2]
+                brand = row[4]
+                quantity = int(row[3])
+                if category not in data:
+                    data[category] = []
+                data[category].append((brand, quantity))
+        return data
+
+    def calculate_averages(self):
+        averages = {}
+        total_orders = sum(len(items) for items in self.data.values())
+        for category, items in self.data.items():
+            total_quantity = sum(quantity for _, quantity in items)
+            num_items = len(items)
+            if num_items > 0:
+                average = total_quantity / total_orders
+                averages[category] = round(average, 2)  # Round the average to 2 decimal places
+        return averages
+
+    def calculate_most_popular_brands(self):
+        brands = {}
+        for category, items in self.data.items():
+            brand_count = {}
+            for brand, _ in items:
+                if brand not in brand_count:
+                    brand_count[brand] = 0
+                brand_count[brand] += 1
+            if brand_count:
+                popular_brand = max(brand_count, key=brand_count.get)
+                brands[category] = popular_brand
+        return brands
+
+    def generate_files(self):
+        averages = self.calculate_averages()
+        most_popular_brands = self.calculate_most_popular_brands()
+
+        # Generate the first file with category averages
+        with open("0_" + self.input_file, "w", newline="") as file:
+            writer = csv.writer(file)
+            for category, average in averages.items():
+                writer.writerow([category, round(average, 2)])
+
+        # Generate the second file with most popular brands
+        with open("1_" + self.input_file, "w", newline="") as file:
+            writer = csv.writer(file)
+            for category, brand in most_popular_brands.items():
+                writer.writerow([category, brand])
+
+        return averages, most_popular_brands
+
+if __name__ == '__main__':
+    # Create an instance of FileGenerator with the input file name
+    generator = FileGenerator('input_example.csv')
+
+    # Generate the files and retrieve the averages and most popular brands
+    generator.generate_files()
--- tests/test_file_generator.py
+++ tests/test_file_generator.py
@@ -1,30 +1,93 @@
 import unittest
 import csv
-
 import os
-import sys
-
-current_directory = os.getcwd()
-sys.path.append(os.path.dirname(current_directory))
-
+from unittest.mock import Mock, call, patch
 from generators.file_generator import FileGenerator
 
 class TestFileGenerator(unittest.TestCase):
-    def setUp(self):
-        self.input_file_name = 'order_log00.csv'
-        self.columns_amount = 4
-        self.generator = FileGenerator(self.input_file_name)
+    @classmethod
+    def setUpClass(cls):
+        # Set up a sample input file for testing
+        cls.input_file = "input_example.csv"
+        data = [
+            ["ID1", "Minneapolis", "shoes", "2", "Air"],
+            ["ID2", "Chicago", "shoes", "1", "Air"],
+            ["ID3", "Central Department Store", "shoes", "5", "BonPied"],
+            ["ID4", "Quail Hollow", "forks", "3", "Pfitzcraft"]
+        ]
 
-    def test_import_files(self):
-        result = self.generator.import_csv()
-        self.assertIsInstance(result, list)
-        for i in range(self.columns_amount):
-            self.assertIsInstance(result[0], list)
+        # Write the sample data to the input file
+        with open(cls.input_file, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
 
-    def test_check_file_format(self):
-        columns_amount = 4
-        result = self.generator.import_csv()
-        self.assertEqual(len(result), columns_amount)
+        # Create an instance of the FileGenerator class for testing
+        cls.generator = FileGenerator(cls.input_file)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Remove the sample input file after testing
+        if os.path.exists("input_example.csv"):
+            os.remove("input_example.csv")
+
+    def test_import_csv(self):
+        # Define the expected data after importing the CSV file
+        expected_data = {
+            "shoes": [("Air", 2), ("Air", 1), ("BonPied", 5)],
+            "forks": [("Pfitzcraft", 3)]
+        }
+
+        # Call the import_csv method and compare the result with the expected data
+        data = self.generator.import_csv()
+        self.assertEqual(data, expected_data)
+
+    def test_calculate_most_popular_brands(self):
+        # Create a test data dictionary
+        data = {
+            "shoes": [("Air", 2), ("Air", 1), ("BonPied", 5)],
+            "forks": [("Pfitzcraft", 3)]
+        }
+
+        # Create a new instance of the FileGenerator class for testing
+        generator = FileGenerator("input_example.csv")
+        generator.data = data
+
+        # Call the calculate_most_popular_brands method and compare the result with the expected result
+        result = generator.calculate_most_popular_brands()
+        expected_result = {"shoes": "Air", "forks": "Pfitzcraft"}
+        self.assertEqual(result, expected_result)
+
+    def test_calculate_averages(self):
+        # Generate the output files and retrieve the averages and most popular brands
+        averages, most_popular_brands = self.generator.generate_files()
+
+        # Test the content of the first generated file
+        with open("0_" + self.generator.input_file, "r") as file:
+            reader = csv.reader(file)
+
+            # Define the expected averages
+            expected_averages = {"shoes": 2.0, "forks": 0.75}
+
+            # Compare each row in the file with the expected average values
+            for row, (category, average) in zip(reader, expected_averages.items()):
+                self.assertEqual(row, [category, str(round(average, 2))])
+
+        # Test the content of the second generated file
+        with open("1_" + self.generator.input_file, "r") as file:
+            reader = csv.reader(file)
+
+            # Define the expected most popular brands
+            expected_brands = {"shoes": "Air", "forks": "Pfitzcraft"}
+
+            # Compare each row in the file with the expected brand values
+            for row, (category, brand) in zip(reader, expected_brands.items()):
+                self.assertEqual(row, [category, brand])
+
+        # Compare the calculated averages and most popular brands with the expected values
+        expected_averages = {"shoes": 2.0, "forks": 0.75}
+        expected_brands = {"shoes": "Air", "forks": "Pfitzcraft"}
+        self.assertEqual(averages, expected_averages)
+        self.assertEqual(most_popular_brands, expected_brands)
 
 
 if __name__ == '__main__':
